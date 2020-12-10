14a1c911cfebbf6325842524a51a7c681a9fd5f1
sAksham-Ar <57193459+sAksham-Ar@users.noreply.github.com>
2020-12-10 21:33:47 +0530

    cleaned code

--- prof.py
+++ prof.py
@@ -12,6 +12,12 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--window-size=1280,800')
 chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
 driver=webdriver.Chrome(r"C:\Users\aryas\Documents\chromedriver.exe",options=chrome_options)
+Key = {'astro-ph': "Astrophysics", 'hep-th': "High Energy Physics Theoretical", 'math-ph': "Mathematical Physics", 'quant-ph': "Quantum Physics",
+       'gr-qc': "General Relativity and Quantum Cosmology", 'hep-ex': "High Energy Physics Experimental", 'hep-ph': "High Energy Physics Phenomenology",
+       'physics': "Physics", 'nucl-th':'Nuclear Theory','hep-lat':'High energy Physics Lattice','nucl-ex':'Nuclear Experiment',
+       'math-ph':'Mathematical Physics','cond-mat':'Condensed Matter','math':"Mathematics",'stat':"Statistics",'physics.acc-ph':"Accelerator Physics",
+       'cs':'Computer Science','nlin':"Non Linear Sciences",'q-bio':"Quantitative Biology",'eess':"Electrical Engineering and Systems Science",
+       'physics.ins-det': 'physics.in-det'}
 for i in range(0,1): 
     driver.get('https://inspirehep.net/authors?sort=bestmatch&size='+str(argv[2])+'&page=1&q='+str(argv[1]))
     author_links=[]
@@ -41,50 +47,8 @@ for i in range(0,1):
             areas=['']
             elements=[]
         for element in elements:
-            if element.text=='astro-ph':
-                areas.append("Astrophysics")
-            elif element.text=='hep-th':
-                areas.append("High Energy Physics Theoretical")
-            elif element.text=='math-ph':
-                areas.append("Mathematical Physics")
-            elif element.text=='quant-ph':
-                areas.append("Quantum Physics")
-            elif element.text=='gr-qc':
-                areas.append("General Relativity and Quantum Cosmology")
-            elif element.text=='hep-ex':
-                areas.append("High Energy Physics Experimental")
-            elif element.text=='hep-ph':
-                areas.append("High Energy Physics Phenomenology")
-            elif element.text=='physics':
-                areas.append(element.text)
-            elif element.text=='nucl-th':
-                areas.append('Nuclear Theory')
-            elif element.text=='hep-lat':
-                areas.append('High energy Physics Lattice')
-            elif element.text=='nucl-ex':
-                areas.append('Nuclear Experiment')
-            elif element.text=='math-ph':
-                areas.append('Mathematical Physics')
-            elif element.text=='cond-mat':
-                areas.append('Condensed Matter')
-            elif element.text=='math':
-                areas.append("Mathematics")
-            elif element.text=='stat':
-                areas.append("Statistics")
-            elif element.text=='physics.acc-ph':
-                areas.append("Accelerator Physics")
-            elif element.text=='cs':
-                areas.append('Computer Science')
-            elif element.text=='nlin':
-                areas.append("Non Linear Sciences")
-            elif element.text=='q-bio':
-                areas.append("Quantitative Biology")
-            elif element.text=='eess':
-                areas.append("Electrical Engineering and Systems Science")
-            elif element.text=='physics.acc-ph':
-                areas.append(element.text)
-            
-
+            if element.text in Key:
+                areas.append(Key[element.text])
         web=''    
         try:
             university=driver.find_element_by_tag_name('h2').find_element_by_tag_name('li').text
